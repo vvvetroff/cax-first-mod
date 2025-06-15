6944d0d8814334d68af589166e344aee3815609b
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2025-06-15 12:37:53 -0400

    Initial commit

---
+++ src/main/java/net/cax/firstmod/CaxsFirstModClient.java
@@ -0,0 +1,10 @@
+package net.cax.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaxsFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cax/firstmod/CaxsFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cax.firstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CaxsFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
